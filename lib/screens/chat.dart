b9515e483ea7f790c0c6f75914beb7b6db574525
cristianliebana <cristian.liebana@estudiantat.upc.edu>
2023-11-23 14:24:56 +0100

    Versión 0.1.16 Chat beta gracias a Jlarrinzal

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,16 +1,155 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:proyecto_flutter/screens/individual_chat.dart';
 import 'package:proyecto_flutter/widget/nav_bar.dart';
-// Importa el archivo nav_bar.dart
 
 class ChatPage extends StatelessWidget {
+  List images = [
+    "assets/chatimages/Christine.jpg",
+    "assets/chatimages/Davis.jpg",
+    "assets/chatimages/Johnson.jpg",
+    "assets/chatimages/Jones Noa.jpg",
+    "assets/chatimages/Parker Bee.jpg",
+    "assets/chatimages/Smith.jpg",
+  ];
+
+  List names = [
+    "Christine",
+    "Davis",
+    "Johnson",
+    "Jones Noa",
+    "Parker Bee",
+    "Smith"
+  ];
+
+  List msgTiming = [
+    "Mon",
+    "12:20",
+    "Sun",
+    "22:20",
+    "05:23",
+    "Wed",
+  ];
+
+  List msgs = [
+    "Hi, How are you?",
+    "Where are you?",
+    "Hello Dear, is all right",
+    "Hello Dear, is all right",
+    "Hello Dear, is all right",
+    "Hello Dear, is all right",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 3),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text('Chat')],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 20, left: 15),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Mensajes",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.search,
+                          size: 35,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "Reciente",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black54,
+                      letterSpacing: 1),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    itemCount: images.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 25),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: AssetImage(images[index]),
+                              minRadius: 30,
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              names[index],
+                              style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 1),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Divider(),
+                SizedBox(height: 20),
+                ListView.builder(
+                  itemCount: images.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => IndividualChat()));
+                      },
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(55),
+                        child: Image.asset(images[index],
+                            height: 55, width: 55, fit: BoxFit.cover),
+                      ),
+                      title: Text(
+                        names[index],
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                      subtitle: Text(names[index]),
+                      trailing: Text(msgTiming[index]),
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/screens/individual_chat.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_flutter/api/services/user_service.dart';
+import 'package:proyecto_flutter/api/utils/http_api.dart';
+import 'package:proyecto_flutter/screens/chat_message_item.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+class ChatMessage {
+  final bool isMeChatting;
+  final String messageBody;
+
+  ChatMessage({required this.isMeChatting, required this.messageBody});
+}
+
+class IndividualChat extends StatefulWidget {
+  const IndividualChat({Key? key}) : super(key: key);
+
+  @override
+  _IndividualChatState createState() => _IndividualChatState();
+}
+
+class _IndividualChatState extends State<IndividualChat> {
+  Map<String, dynamic> userData = {};
+  late io.Socket socket;
+  TextEditingController messageController = TextEditingController();
+  List<ChatMessage> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    connectToSocket();
+    obtenerDatosUsuario();
+  }
+
+  Future<void> obtenerDatosUsuario() async {
+    ApiResponse response = await UserService.getUserById();
+    setState(() {
+      userData = response.data;
+      print(userData);
+    });
+  }
+
+  void connectToSocket() {
+    socket = io.io('http://localhost:9090', <String, dynamic>{
+      'transports': ['websocket'],
+    });
+
+    socket.on('connect', (_) {
+      print('Conectado al servidor Socket.IO');
+    });
+
+    socket.on('chat message', (data) {
+      String senderId = data['userId'];
+      String message = data['message'];
+      bool isMe = senderId == userData['_id'];
+
+      setState(() {
+        messages.add(ChatMessage(isMeChatting: isMe, messageBody: message));
+      });
+    });
+
+    socket.on(
+        'disconnect', (_) => print('Desconectado del servidor Socket.IO'));
+  }
+
+  void sendMessage() {
+    String message = messageController.text;
+    if (message.isNotEmpty) {
+      Map<String, dynamic> messageData = {
+        'message': message,
+        'userId': userData['_id'],
+      };
+
+      socket.emit('chat message', messageData);
+
+      // Mover la adición del mensaje a la lista dentro del callback del evento 'chat message'
+      // para evitar duplicaciones.
+      // setState(() {
+      //   messages.add(ChatMessage(isMeChatting: true, messageBody: message));
+      // });
+
+      messageController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              height: 85,
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  BackButton(),
+                  SizedBox(width: 5),
+                  CircleAvatar(
+                    backgroundImage:
+                        AssetImage("assets/chatimages/Jones Noa.jpg"),
+                    maxRadius: 28,
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Jones Noa",
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Active 5 hours ago",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.grey.shade200,
+                child: ListView(
+                  padding: EdgeInsets.all(15),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Today",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    for (var message in messages)
+                      Align(
+                        alignment: message.isMeChatting
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: ChatMessageItem(
+                          isMeChatting: message.isMeChatting,
+                          messageBody: message.messageBody,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(13),
+        ),
+        child: Container(
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: messageController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Escribe algo...",
+                    hintStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  maxLines: 10,
+                  minLines: 1,
+                ),
+              ),
+              SizedBox(width: 20),
+              InkWell(
+                onTap: () {
+                  sendMessage();
+                },
+                hoverColor: Colors.white,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[900],
+                    borderRadius: BorderRadius.circular(13),
+                  ),
+                  alignment: Alignment.center,
+                  child:
+                      Icon(Icons.send_rounded, color: Colors.white, size: 25),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
