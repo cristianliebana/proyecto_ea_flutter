aecbd1aeb1b3a7fe20c3c9d40ce4b3982cd5895c
Omar <omar.ferrer@neutroon.com>
2023-12-10 10:28:40 +0100

    V 0.2.2 - user profile

---
+++ lib/screens/user_profile.dart
@@ -0,0 +1,350 @@
+import 'package:cloudinary_flutter/cloudinary_context.dart';
+import 'package:cloudinary_flutter/image/cld_image.dart';
+import 'package:cloudinary_url_gen/cloudinary.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:get/get.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:lottie/lottie.dart';
+import 'package:proyecto_flutter/api/models/product_model.dart';
+import 'package:proyecto_flutter/api/services/product_service.dart';
+import 'package:proyecto_flutter/api/services/rating_service.dart';
+import 'package:proyecto_flutter/api/services/token_service.dart';
+import 'package:proyecto_flutter/api/services/user_service.dart';
+import 'package:proyecto_flutter/api/utils/http_api.dart';
+import 'package:proyecto_flutter/screens/chat.dart';
+import 'package:proyecto_flutter/screens/product_detail.dart';
+import 'package:proyecto_flutter/screens/profile.dart';
+import 'package:proyecto_flutter/screens/update_user.dart';
+import 'package:proyecto_flutter/screens/user_products.dart';
+import 'package:proyecto_flutter/utils/constants.dart';
+import 'package:proyecto_flutter/utils/theme_provider.dart';
+import 'package:proyecto_flutter/widget/nav_bar.dart';
+import 'package:proyecto_flutter/widget/profile_tab_bar.dart';
+
+
+class UserProfileScreen extends StatefulWidget {
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  List<Product> products = [];
+  Map<String, dynamic> userData = {};
+  
+  late ScrollController _scrollController;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    obtenerDatosUsuario();
+  }
+
+  Future<void> obtenerDatosUsuario() async {
+    ApiResponse response = await UserService.getUserById();
+    setState(() {
+      userData = response.data;
+      print(userData);
+      print(userData['_id']);
+      loadUserProducts(userData['_id']);
+    });
+  }
+
+  Future<void> loadUserProducts(String? userId) async {
+    if (userId != null) {
+      final List<Product> userproducts =
+          await ProductService.getUserProducts(userId);
+      setState(() {
+        products = userproducts;
+        print(products);
+      });
+    } else {
+      print('UserId is null.');
+    }
+  }
+
+/*  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      _loadMoreProducts();
+    }
+  }*/
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      actions: [
+        _buildAppBarThemeButton(),
+      ],
+    );
+  }
+
+  Widget _buildAppBarThemeButton() {
+    final ThemeProvider themeProvider = Get.find<ThemeProvider>();
+
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.onPrimary,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        icon: Icon(
+          themeProvider.isDarkMode ? Icons.dark_mode : Icons.light_mode,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        onPressed: () {
+          // Lógica para cambiar el tema
+          Get.find<ThemeProvider>().toggleTheme();
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 5),
+      body: Column(
+        children: [
+          // Sección de información del usuario (arriba)
+          SizedBox(height: 10),
+          ProfileImage(userData: userData),
+          const SizedBox(height: 10),
+          if (userData.isNotEmpty) UsernameText(userData: userData),
+          const SizedBox(height: 10),
+          if (userData.isNotEmpty) EmailText(userData: userData),
+          if (userData.isEmpty) CircularProgressIndicator(),
+          const SizedBox(height: 10),
+          const SizedBox(height: 10),
+          ProfileTabBar(currentIndex: 0),
+          const SearchBar(),
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return ProductsVerticalItem(product: products[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          const SizedBox(height: 10), 
+          Padding(
+            padding: const EdgeInsets.only(),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                filled: true,
+                fillColor: Theme.of(context).colorScheme.onPrimary,
+                prefixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                hintText: "Busca en productos",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductsVerticalItem extends StatelessWidget {
+  final Product product;
+
+  const ProductsVerticalItem({Key? key, required this.product})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Get.to(ProductDetailScreen(productId: product.id ?? ''));
+      },
+      child: Container(
+        margin: EdgeInsets.only(
+            left: gWidth * 0.04, top: gHeight * 0.02, right: gWidth * 0.04),
+        width: gWidth / 1.5,
+        height: gHeight / 4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: product.productImage != null &&
+                          product.productImage!.isNotEmpty
+                      ? NetworkImage(product.productImage!.first)
+                      : AssetImage('assets/images/profile.png')
+                          as ImageProvider, // Use the image URL
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 10,
+              left: 20,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  product.name ?? '',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  '${product.price} €/Kg', // Agrega el precio del producto
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EmailText extends StatelessWidget {
+  const EmailText({
+    Key? key,
+    required this.userData,
+  });
+
+  final Map<String, dynamic> userData;
+
+  @override
+  Widget build(BuildContext context) {
+    String email = userData['email'] ?? "N/A";
+
+    return Text(
+      email != "N/A" ? "$email" : "",
+      style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w200,
+          color: Theme.of(context).primaryColor),
+    );
+  }
+}
+
+class UsernameText extends StatelessWidget {
+  const UsernameText({
+    Key? key,
+    required this.userData,
+  });
+
+  final Map<String, dynamic> userData;
+
+  @override
+  Widget build(BuildContext context) {
+    String username = userData['username'] ?? "N/A";
+
+    return Text(
+      username != "N/A" ? "$username" : "",
+      style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Theme.of(context).primaryColor),
+    );
+  }
+}
+
+class ProfileImage extends StatelessWidget {
+  const ProfileImage({
+    Key? key,
+    required this.userData,
+  }) : super(key: key);
+
+  final Map<String, dynamic> userData;
+
+  @override
+  Widget build(BuildContext context) {
+    String profileImage = userData['profileImage'] ?? "";
+
+    return Container(
+      width: 175,
+      height: 175,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: CldImageWidget(
+          publicId: profileImage.isNotEmpty
+              ? profileImage
+              : "https://res.cloudinary.com/dfwsx27vx/image/upload/v1701028188/profile_ju3yvo.png",
+          width: 175,
+          height: 175,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+
