df607d043c1fc703746e7e0b68b4d6b1dafe9864
Omar <omar.ferrer@neutroon.com>
2024-01-13 22:09:52 +0100

    reviews fix from cristian and jose added

--- lib/screens/user_profile_reviews.dart
+++ lib/screens/user_profile_reviews.dart
@@ -8,6 +8,7 @@ import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:lottie/lottie.dart';
 import 'package:proyecto_flutter/api/models/product_model.dart';
 import 'package:proyecto_flutter/api/models/rating_model.dart';
+import 'package:proyecto_flutter/api/models/user_model.dart';
 import 'package:proyecto_flutter/api/services/product_service.dart';
 import 'package:proyecto_flutter/api/services/rating_service.dart';
 import 'package:proyecto_flutter/api/services/token_service.dart';
@@ -18,15 +19,16 @@ import 'package:proyecto_flutter/screens/product_detail.dart';
 import 'package:proyecto_flutter/screens/profile.dart';
 import 'package:proyecto_flutter/screens/update_user.dart';
 import 'package:proyecto_flutter/screens/user_products.dart';
+import 'package:proyecto_flutter/screens/user_profile.dart';
 import 'package:proyecto_flutter/utils/constants.dart';
 import 'package:proyecto_flutter/utils/theme_provider.dart';
 import 'package:proyecto_flutter/widget/nav_bar.dart';
 import 'package:proyecto_flutter/widget/profile_tab_bar.dart';
 
-
 class UserProfileReviewsScreen extends StatefulWidget {
   @override
-  _UserProfileReviewsScreenState createState() => _UserProfileReviewsScreenState();
+  _UserProfileReviewsScreenState createState() =>
+      _UserProfileReviewsScreenState();
 }
 
 class _UserProfileReviewsScreenState extends State<UserProfileReviewsScreen> {
@@ -34,7 +36,8 @@ class _UserProfileReviewsScreenState extends State<UserProfileReviewsScreen> {
   List<Rating> ratings = [];
   Map<String, dynamic> userData = {};
   Map<String, dynamic> userData2 = {};
-  
+  List<Map<String, dynamic>> allUserData2 = [];
+
   late ScrollController _scrollController;
   bool _loading = false;
 
@@ -49,50 +52,74 @@ class _UserProfileReviewsScreenState extends State<UserProfileReviewsScreen> {
     ApiResponse response = await UserService.getUserById();
     setState(() {
       userData = response.data;
+      loadUserProducts(userData['_id']);
       loadUserRatings(userData['_id']);
     });
   }
 
+  Future<void> loadUserProducts(String? userId) async {
+    if (userId != null) {
+      final List<Product> userproducts =
+          await ProductService.getUserProducts(userId);
+      setState(() {
+        products = userproducts;
+        print(products);
+      });
+    } else {
+      print('UserId is null.');
+    }
+  }
 
   Future<void> loadUserRatings(String? userId) async {
-  if (userId != null) {
-    final List<Rating> userratings = await RatingService.getUserRatings(userId);
-    setState(() {
-      ratings = userratings;
-      print(ratings);
-      obtenerDatosCreadorReview(ratings);
-    });
-  } else {
-    print('UserId is null.');
+    if (userId != null) {
+      final List<Rating> userratings =
+          await RatingService.getUserRatings(userId);
+      setState(() {
+        ratings = userratings;
+        print('Ratings: $ratings');
+        obtenerDatosCreadorReview(ratings);
+      });
+    } else {
+      print('UserId is null.');
+    }
   }
-}
 
-Future<void> obtenerDatosCreadorReview (List<Rating> ratings) async{
-   for (var rating in ratings) {
+  Future<void> obtenerDatosCreadorReview(List<Rating> ratings) async {
+    for (var rating in ratings) {
       if (rating.userId1 != null) {
-        final userData2 = await UserService.getCreadorById(rating.userId1!);
-        print(userData2);
+        print('UserId1: ${rating.userId1}');
+        final response = await UserService.getCreadorById(rating.userId1!);
+
+        setState(() {
+          allUserData2.add(
+              response.data); // Agregar datos del creador actual a la lista
+        });
+
+        print('UserData2: ${response.data}');
       }
     }
-}
 
-/*  void _scrollListener() {
-    if (_scrollController.position.pixels ==
-        _scrollController.position.maxScrollExtent) {
-      _loadMoreProducts();
-    }
-  }*/
+    // Al finalizar el bucle, puedes usar allUserData que tiene los datos de todos los creadores
+    print('Todos los datos de los creadores: $allUserData2');
+  }
 
   @override
   void dispose() {
     _scrollController.dispose();
     super.dispose();
   }
+
   AppBar _buildAppBar() {
     return AppBar(
       elevation: 0,
       backgroundColor: Colors.transparent,
       centerTitle: true,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Get.to(ProfilePage());
+        },
+      ),
       actions: [
         _buildAppBarThemeButton(),
       ],
@@ -125,7 +152,6 @@ Future<void> obtenerDatosCreadorReview (List<Rating> ratings) async{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 5),
       body: Column(
         children: [
           // Sección de información del usuario (arriba)
@@ -144,7 +170,11 @@ Future<void> obtenerDatosCreadorReview (List<Rating> ratings) async{
               controller: _scrollController,
               itemCount: ratings.length,
               itemBuilder: (context, index) {
-                return RatingsListItem(rating: ratings[index], userData2: userData2);
+                return RatingsListItem(
+                  rating: ratings[index],
+                  alluserData2: allUserData2,
+                  index: index, // Agrega el índice aquí
+                );
               },
             ),
           ),
@@ -154,7 +184,54 @@ Future<void> obtenerDatosCreadorReview (List<Rating> ratings) async{
   }
 }
 
+class SearchBar extends StatelessWidget {
+  const SearchBar({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                filled: true,
+                fillColor: Theme.of(context).colorScheme.onPrimary,
+                prefixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                hintText: "Busca en productos",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class EmailText extends StatelessWidget {
   const EmailText({
@@ -230,62 +307,77 @@ class ProfileImage extends StatelessWidget {
   }
 }
 
-
 class RatingsListItem extends StatelessWidget {
   final Rating rating;
-  final Map<String, dynamic> userData2;
-  const RatingsListItem({Key? key, required this.rating, required this.userData2}) : super(key: key);
-  
-  
+  final List<Map<String, dynamic>> alluserData2;
+  final int index;
 
-@override
-Widget build(BuildContext context) {
-  return Container(
-    margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-    padding: EdgeInsets.all(16.0),
-    decoration: BoxDecoration(
-      color: Theme.of(context).colorScheme.onPrimary,
-      borderRadius: BorderRadius.circular(15.0),
-    ),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleAvatar(
-          radius: 35,
-          backgroundImage: userData2['profileImage'] != null &&
-                  userData2['profileImage'].isNotEmpty
-              ? NetworkImage(userData2['profileImage']!)
-              : Image.asset('assets/images/profile.png').image,
-          backgroundColor: Colors.transparent,
+  RatingsListItem({
+    Key? key,
+    required this.rating,
+    required this.alluserData2,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (index >= 0 && index < alluserData2.length) {
+      return Container(
+        margin: EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.onPrimary,
+          borderRadius: BorderRadius.circular(15.0),
         ),
-        SizedBox(width: 16.0),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 35,
+              backgroundImage: alluserData2[index]['profileImage'] != null &&
+                      alluserData2[index]['profileImage'].isNotEmpty
+                  ? NetworkImage(alluserData2[index]['profileImage']!)
+                  : Image.asset('assets/images/profile.png').image,
+              backgroundColor: Colors.transparent,
+            ),
+            SizedBox(width: 16.0),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  RatingBar(
-                    ignoreGestures: true,
-                    initialRating: rating.rating ?? 3.5,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    ratingWidget: RatingWidget(
-                      full: Image.asset(
-                          'assets/ratingimages/zanahoriaentera.png'),
-                      half: Image.asset(
-                          'assets/ratingimages/mediazanahoria.png'),
-                      empty: Image.asset(
-                          'assets/ratingimages/zanahoriavacia.png'),
-                    ),
-                    itemPadding:
-                        EdgeInsets.symmetric(horizontal: 4.0),
-                    onRatingUpdate: (rating) {},
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      RatingBar(
+                        ignoreGestures: true,
+                        initialRating: rating.rating ?? 3.5,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        ratingWidget: RatingWidget(
+                          full: Image.asset(
+                              'assets/ratingimages/zanahoriaentera.png'),
+                          half: Image.asset(
+                              'assets/ratingimages/mediazanahoria.png'),
+                          empty: Image.asset(
+                              'assets/ratingimages/zanahoriavacia.png'),
+                        ),
+                        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                        onRatingUpdate: (rating) {},
+                      ),
+                      Text(
+                        alluserData2[index]['username'] ?? '',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
                   ),
+                  SizedBox(height: 8.0),
+                  // Comment
                   Text(
-                    userData2['username']?? '',
+                    rating.comment ?? '',
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.primary,
                       fontSize: 16.0,
@@ -293,21 +385,15 @@ Widget build(BuildContext context) {
                   ),
                 ],
               ),
-              SizedBox(height: 8.0),
-              // Comment
-              Text(
-                rating.comment ?? '',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 16.0,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
-    ),
-  );
-}}
+      );
+    } else {
+      // Si el índice está fuera de rango, puedes retornar un widget vacío o manejarlo de alguna otra manera.
+      return Container();
+    }
+  }
+}
 
 
